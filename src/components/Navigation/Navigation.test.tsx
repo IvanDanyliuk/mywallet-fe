ab6ca771ceb0cbc6f31f48047042a638eb8ea392
IvanDanyliuk <ivandaniliuk@gmail.com>
2022-06-28 19:12:02 +0300

    Navigation component has been covered with tests

--- src/components/Navigation/Navigation.test.tsx
+++ src/components/Navigation/Navigation.test.tsx
@@ -6,6 +6,34 @@ import store from '../../redux/store';
 import Navigation from './Navigation';
 
 
+const mockedStore = {
+  ...store.getState(),
+  user: {
+    user: {
+      _id: '6295f2be26cf5e82fcc3b1ca',
+      firstName: 'Johnny',
+      lastName: 'Doe',
+      email: 'j.doe@gmail.com',
+      password: '$2a$12$VSqDjXVvCD0d7bl9sXVGUeTQOmt33f303Hr7GuppVT2.NulxaCoJ.',
+      avatar: 'https://firebasestorage.googleapis.com/v0/b/my-wallet-be092.appspot.com/o/files%2Favatar.jpg?alt=media&token=a70f3ac4-20fb-454a-ac24-6781c855bc72',
+      language: 'ua',
+      currency: 'usd',
+      __v: 0,
+    },
+    status: 'succeeded',
+    error: null,
+  },
+};
+
+store.getState = () => mockedStore;
+
+const mockedUseDispatch = jest.fn();
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: () => mockedUseDispatch,
+}));
+
+
 describe('Navigation tests', () => {
   beforeEach(() => {
     render(
@@ -18,6 +46,15 @@ describe('Navigation tests', () => {
   });
 
   it('should render the Navigation component', () => {
-    screen.debug()
-  })
+    const navLinks = screen.getAllByRole('link');
+    expect(navLinks.length).toBe(6);
+  });
+
+  it('should change the page after clicking the logout button', async () => {
+    const logoutBtn = screen.getByRole('button', { name: 'logoutBtn' });
+    fireEvent.click(logoutBtn);
+    await waitFor(() => {
+      expect(mockedUseDispatch).toHaveBeenCalled();
+    });
+  });
 });
